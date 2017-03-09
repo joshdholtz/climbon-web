595d7225d67c3a0e33b163ed6ffc829620212205
Josh Holtz <josh@rokkincat.com>
2017-03-08 23:31:53 -0600

    Route and reviews views are somewhat there

--- src/components/reviews.js
+++ src/components/reviews.js
@@ -1,15 +1,66 @@
 import React, { Component } from 'react';
-import { Grid, Row, Col, ControlLabel } from 'react-bootstrap';
+import { Grid, Row, Col, Image } from 'react-bootstrap';
+
+import { makeRequest, checkStatusJSON } from '../api'
 
 export default class Reviews extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      reviews: []
+    };
+  }
+  
+  componentWillMount() {
+    this.fetchReviews()
+  }
+  
+  fetchReviews() {
+    return makeRequest('/api/reviews')
+      .then(checkStatusJSON)
+      .then( (reviews) => {
+        this.setState({
+          reviews: reviews
+        })
+      })
+      .catch( (ex) => {
+        this.setState({
+          reviews: []
+        })
+      })
+  }
+  
+  renderReview = (review) => {
+    let title = review.title && ( <div className="title">{review.title}</div> )
+    let rating = review.rating && ( <div className="review-info">Rating: {review.rating}</div> )
+    let suggestedGrade = review.suggested_grade && ( <div className="review-info">Suggested Grade: {review.suggested_grade}</div> )
+    
+    return (
+      <Row>
+        <Col smOffset={1} sm={10} className="review">
+          <Row>
+            <Col sm={4}>
+              <Image responsive src={"https://akamai-anprod.active.com/godleypark/servlet/downloadFile.sdi?uploadedfile_id=262"} />
+            </Col>
+            <Col sm={8}>
+              {title}
+              {rating}
+              {suggestedGrade}
+            </Col>
+          </Row>
+        </Col>
+      </Row>
+    )
+  }
+  
   render() {
     return (
-      <Grid>
-        <Row>
-          <Col sm={4}>Reviews</Col>
-          <Col sm={4}>B</Col>
-          <Col sm={4}>C</Col>
-        </Row>
+      <Grid className="page-reviews">
+        {
+          this.state.reviews.map((review) => {
+             return this.renderReview(review)
+          })
+        }
       </Grid>
     );
   }
--- src/components/routes.js
+++ src/components/routes.js
@@ -1,15 +1,77 @@
 import React, { Component } from 'react';
-import { Grid, Row, Col, ControlLabel } from 'react-bootstrap';
+import { Grid, Row, Col, Image } from 'react-bootstrap';
+
+import { makeRequest, checkStatusJSON } from '../api'
 
 export default class Routes extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      routes: []
+    };
+  }
+  
+  componentWillMount() {
+    this.fetchRoutes()
+  }
+  
+  fetchRoutes() {
+    return makeRequest('/api/routes')
+      .then(checkStatusJSON)
+      .then( (routes) => {
+        this.setState({
+          routes: routes
+        })
+      })
+      .catch( (ex) => {
+        this.setState({
+          locations: []
+        })
+      })
+  }
+  
+  renderRoute = (route) => {
+    let name = route.name && ( <div className="name">{route.name}</div> )
+    let grade = route.grade && ( <div className="route-info">Grade: {route.grade}</div> )
+    let setter = route.setter && ( <div className="route-info">Setter: {route.setter}</div> )
+    let type = route.type && ( <div className="route-info">Type: {route.type}</div> )
+    let info = route.info && ( <div className="route-info">Info: {route.info}</div> )
+    
+    let averageRating = ( <div className="rating-info">Avg Rating: 4.5/5</div> )
+    let numberOfReviews = ( <div className="rating-info">11 Reviews</div> )
+    
+    return (
+      <Row>
+        <Col smOffset={1} sm={10} className="route">
+          <Row>
+            <Col sm={4}>
+              <Image responsive src={"https://akamai-anprod.active.com/godleypark/servlet/downloadFile.sdi?uploadedfile_id=262"} />
+            </Col>
+            <Col sm={4}>
+              {name}
+              {grade}
+              {setter}
+              {type}
+              {info}
+            </Col>
+            <Col sm={4}>
+              {averageRating}
+              {numberOfReviews}
+            </Col>
+          </Row>
+        </Col>
+      </Row>
+    )
+  }
+  
   render() {
     return (
-      <Grid>
-        <Row>
-          <Col sm={4}>Routes</Col>
-          <Col sm={4}>B</Col>
-          <Col sm={4}>C</Col>
-        </Row>
+      <Grid className="page-routes">
+        {
+          this.state.routes.map((route) => {
+             return this.renderRoute(route)
+          })
+        }
       </Grid>
     );
   }
