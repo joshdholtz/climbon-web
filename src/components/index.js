954c170fd5bc458727821a2821c1b34f58c825bb
Josh Holtz <josh@rokkincat.com>
2017-03-11 14:06:35 -0600

    Mailchimp goodness

--- src/components/index.js
+++ src/components/index.js
@@ -3,6 +3,26 @@ import React, { Component } from 'react';
 import logo from '../images/logo.png'
 
 export default class Index extends Component {
+  renderMailchimpEarlyAccess() {
+    const hiddenInputStyle = {
+      position: 'absolute',
+      left: '-5000px'
+    }
+    
+    return (
+      <div className="mailchimp">
+        <div id="mc_embed_signup">
+          <form action="//climbonapp.us15.list-manage.com/subscribe/post?u=bd11d40eab151332ccc867ab7&amp;id=5348af74da" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" target="_blank">
+            <div className="form-info">Subscribe For Early Access Info</div>
+            <input type="email" name="EMAIL" className="required email" id="mce-EMAIL" placeholder="Your Email"/>
+            <input style={hiddenInputStyle} type="text" name="b_bd11d40eab151332ccc867ab7_5348af74da" tabindex="-1" value=""/>
+            <input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" className="button"/>
+          </form>
+        </div>
+      </div>
+    )
+  }
+  
   render() {
     return (
       <div>
@@ -16,6 +36,7 @@ export default class Index extends Component {
                             <hr className="star-light"/>
                             <span className="skills">Climbing Journal - Review Routes - Analyze Data</span>
                         </div>
+                        {this.renderMailchimpEarlyAccess()}
                     </div>
                 </div>
             </div>
